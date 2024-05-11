15d53aa46f16d840fc7d2ae2d64f7a84bee3750f
Henry <henrytran012599@gmail.com>
2024-05-12 01:41:43 +0700

    cmnewday

--- src/ui/SearchProducts.jsx
+++ src/ui/SearchProducts.jsx
@@ -1,24 +1,51 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const ListItem = styled.ul`
+  list-style-type: none;
   display: flex;
   flex-wrap: wrap;
 `;
 
 const Item = styled.li`
-  list-style-type: none;
+  display: flex;
+  flex-direction: column;
   width: 20%;
-  height: 200px;
+  height: 250px;
+  padding-left: 0.3125rem;
+  padding-right: 0.3125rem;
+  margin: 0.3125rem 0px;
+  flex-basis: 20%;
+  max-width: 20%;
+  flex: 0 0 auto;
 `;
 
-const Img = styled.div`
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  border: 0px solid rgb(229, 231, 235);
+  box-sizing: border-box;
+`;
+
+const Img = styled.img`
   background-image: url(${(props) => props.source});
-  background-position: center;
+  background-size: contain;
   background-repeat: no-repeat;
   background-size: cover;
-  width: 186.4px;
-  height: 186.4px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.div`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 100px;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  min-height: 2.5rem;
+  cursor: pointer;
 `;
 
 function SearchProducts({ data }) {
@@ -27,9 +54,11 @@ function SearchProducts({ data }) {
     <ListItem>
       {data.map((item) => (
         <Item key={item.item_id}>
-          <Img source={item.img} />
-          <div>{item.title}</div>
+          <ImageContainer>
+            <Img source={item.img} />
+          </ImageContainer>
           <div>
+            <Title>{item.title}</Title>
             <div>{item.price_info.price}</div>
             <div>{item.shop_info.shop_location}</div>
           </div>
